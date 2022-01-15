30c50d71d9dca394090108d80beb9afd2da33176
mcpu3 <mcpu3@outlook.jp>
2022-01-16 01:42:33 +0900

    refactoring

--- js/bio.js
+++ js/bio.js
@@ -34,6 +34,11 @@ window.addEventListener('mousemove', (event) => {
     perspectivate_camera.position.set(0.5 * (window.innerWidth / 4) * (event.clientX / window.innerWidth * 2 - 1), 0.5 * (window.innerHeight / 4) * (-event.clientY / window.innerHeight * 2 + 1), perspectivate_camera_position_z());
     perspectivate_camera.lookAt(0.5 * (window.innerWidth / 4) * (event.clientX / window.innerWidth * 2 - 1), 0.5 * (window.innerHeight / 4) * (-event.clientY / window.innerHeight * 2 + 1), 0);
 });
+window.addEventListener('touchmove', (event) => {
+    console.log(event.touches[0].clientX);
+    perspectivate_camera.position.set(0.5 * (window.innerWidth / 4) * (event.touches[0].clientX / window.innerWidth * 2 - 1), 0.5 * (window.innerHeight / 4) * (-event.touches[0].clientY / window.innerHeight * 2 + 1), perspectivate_camera_position_z());
+    perspectivate_camera.lookAt(0.5 * (window.innerWidth / 4) * (event.touches[0].clientX / window.innerWidth * 2 - 1), 0.5 * (window.innerHeight / 4) * (-event.touches[0].clientY / window.innerHeight * 2 + 1), 0);
+});
 
 function update() {
     perspectivate_camera.position.z = perspectivate_camera_position_z();
